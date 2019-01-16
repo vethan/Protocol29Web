df33ab76975debb417fb850e3a8d9f18bae018a6
Nathan John <vethan@unrulyattractions.com>
2019-01-16 17:09:12 +0000

    Protocol 29 starts

--- protocol29/src/App.js
+++ protocol29/src/App.js
@@ -1,26 +1,65 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  Box,
+  Button,
+  Collapsible,
+  Heading,
+  Grommet,
+  ResponsiveContext,
+} from 'grommet';
+
+const theme = {
+  global: {
+    colors: {
+      "brand": '#222220',
+      "dark": '#000000',
+      text: {
+        "dark": '#80FF00'
+      },
+      control: {
+        "dark": '#80FF00'
+      }
+
+    },
+    font: {
+      family: 'Source Code Pro',
+      size: '14px',
+      height: '20px',
+    },
+  },
+};
+
+const AppBar = (props) => (
+  <Box
+    tag='header'
+    direction='row'
+    align='center'
+    justify='between'
+    background='black'
+    pad={{ left: 'medium', right: 'small', vertical: 'small' }}
+    elevation='medium'
+    style={{ zIndex: '1' }}
+    {...props}
+  />
+);
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <Grommet theme={theme} full>
+        <Box fill>
+          <AppBar>
+            <Heading level='3' margin='none'>Protocol 29</Heading>
+          </AppBar>
+          <Box direction='row' flex overflow={{ horizontal: 'hidden' }}>
+            <Box flex align='center' justify='center' background='black'>
+              <Heading level='1' alignSelf='center' textAlign='center' margin='none'>Protocol 29 initialising...</Heading>
+
+            </Box>
+          </Box>
+        </Box>
+
+      </Grommet >
     );
   }
 }
