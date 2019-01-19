9b84681dd256f368c09332758d7e67a00b780c4e
Nathan John <vethan@unrulyattractions.com>
2019-01-19 17:49:48 +0000

    Almost ready for stage 0

--- protocol29/src/App.js
+++ protocol29/src/App.js
@@ -5,7 +5,10 @@ import {
   Collapsible,
   Heading,
   Grommet,
+  Meter,
   ResponsiveContext,
+  TextInput,
+  Text,
 } from 'grommet';
 
 const theme = {
@@ -13,6 +16,7 @@ const theme = {
     colors: {
       "brand": '#222220',
       "dark": '#000000',
+      "fail": '#bb0000',
       text: {
         "dark": '#80FF00'
       },
@@ -44,19 +48,69 @@ const AppBar = (props) => (
 );
 
 class App extends Component {
+  state = {
+    foxesScore: 0,
+    archersScore: 0,
+    idState: 'none',
+    selectedId: "Someid"
+  }
+
   render() {
+    var scoreSection = (
+      <Box direction='row' alignSelf='center' gap='xlarge' pad='xlarge' overflow={{ horizontal: 'hidden' }}>
+        <Box align='center' justify='center' background='black'>
+          <Meter alignSelf='center' round='true' type='circle' size="xsmall" thickness="small" values={[{ value: this.state.foxesScore, label: "foxes" }]} />
+          <Heading level='3' margin='none'>Foxes</Heading>
+        </Box>
+
+
+        <Box align='center' justify='center' background='black'>
+          <Meter alignSelf='center' round='true' type='circle' size="xsmall" thickness="small" values={[{ value: this.state.archersScore, label: "archers" }]} />
+          <Heading level='3' margin='none'>Archers</Heading>
+        </Box>
+      </Box>
+    )
+
+    var uninit;
+    if (this.state.idState == 'none') {
+      uninit = (
+        <Box align='center' justify='center' background='black' gap='small'>
+          <Heading level='1' alignSelf='center' textAlign='center' margin='none'>Enter your Protocol 29 id:</Heading>
+          <TextInput placeholder="Enter id here" alignSelf='center' textAlign='center' margin='none' />
+          <Button label="Enter" />
+        </Box>
+      )
+    } else if (this.state.idState == 'failed') {
+      uninit = (
+        <Box align='center' justify='center' background='black' gap='small'>
+          <Heading level='1' alignSelf='center' textAlign='center' margin='none'>Enter your Protocol 29 id:</Heading>
+          <TextInput placeholder="Enter id here" alignSelf='center' textAlign='center' margin='none' />
+          <Text level='4' color="fail" alignSelf='center' textAlign='center' margin='none'>Failed! {this.state.selectedId} doesn't exist</Text>
+          <Button label="Enter" />
+        </Box>
+      )
+    } else if (this.state.idState == 'found') {
+      //TODO:  Display the team + Name + ID
+      uninit = (
+        <Box align='center' justify='center' background='black' gap='small'>
+          <Heading level='1' alignSelf='center' textAlign='center' margin='none'>Enter your Protocol 29 id:</Heading>
+          <TextInput placeholder="Enter id here" alignSelf='center' textAlign='center' margin='none' />
+          <Text level='4' color="fail" alignSelf='center' textAlign='center' margin='none'>Failed! {this.state.selectedId} doesn't exist</Text>
+          <Button label="Enter" />
+        </Box>
+      )
+    }
     return (
       <Grommet theme={theme} full>
-        <Box fill>
+        <Box fill background='black'>
           <AppBar>
             <Heading level='3' margin='none'>Protocol 29</Heading>
           </AppBar>
-          <Box direction='row' flex overflow={{ horizontal: 'hidden' }}>
-            <Box flex align='center' justify='center' background='black'>
-              <Heading level='1' alignSelf='center' textAlign='center' margin='none'>Protocol 29 initialising...</Heading>
+          {scoreSection}
+          {uninit}
 
-            </Box>
-          </Box>
+
+
         </Box>
 
       </Grommet >
