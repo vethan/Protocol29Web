2b71614d47bcdce3b9e6636b4600a38c653e231c
Nathan John <vethan@unrulyattractions.com>
2019-01-20 20:12:02 +0000

    Can pull your information

--- protocol29/src/App.js
+++ protocol29/src/App.js
@@ -52,50 +52,73 @@ class App extends Component {
     foxesScore: 0,
     archersScore: 0,
     idState: 'none',
-    selectedId: "Someid"
+    selectedId: "Someid",
+    playerName: "Jackson",
+    team: "archers",
+    userInput: ""
+  }
+
+  toTitleCase(str) {
+    return str.replace(/\w\S*/g, function (txt) {
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+  }
+
+  sendThing() {
+    if (this.state.userInput) {
+      fetch("http://protocol29.gear.host/user/" + this.state.userInput.toUpperCase()).then(data => { return data.json() }).then(res => {
+        if (res.success === false) {
+          this.setState({ idState: "failed" })
+        } else {
+          this.setState({ idState: "found", playerName: res.name, selectedId: res.id, team: res.team })
+        }
+        console.log(res)
+      })
+    }
+    // this.setState({ idState: 'found' })
   }
 
   render() {
     var scoreSection = (
       <Box direction='row' alignSelf='center' gap='xlarge' pad='xlarge' overflow={{ horizontal: 'hidden' }}>
         <Box align='center' justify='center' background='black'>
-          <Meter alignSelf='center' round='true' type='circle' size="xsmall" thickness="small" values={[{ value: this.state.foxesScore, label: "foxes" }]} />
+          <Meter alignSelf='center' round={true} type='circle' size="xsmall" thickness="small" values={[{ value: this.state.foxesScore, label: "foxes" }]} />
           <Heading level='3' margin='none'>Foxes</Heading>
         </Box>
 
 
         <Box align='center' justify='center' background='black'>
-          <Meter alignSelf='center' round='true' type='circle' size="xsmall" thickness="small" values={[{ value: this.state.archersScore, label: "archers" }]} />
+          <Meter alignSelf='center' round={true} type='circle' size="xsmall" thickness="small" values={[{ value: this.state.archersScore, label: "archers" }]} />
           <Heading level='3' margin='none'>Archers</Heading>
         </Box>
       </Box>
     )
 
     var uninit;
-    if (this.state.idState == 'none') {
+    if (this.state.idState === 'none') {
       uninit = (
         <Box align='center' justify='center' background='black' gap='small'>
           <Heading level='1' alignSelf='center' textAlign='center' margin='none'>Enter your Protocol 29 id:</Heading>
-          <TextInput placeholder="Enter id here" alignSelf='center' textAlign='center' margin='none' />
-          <Button label="Enter" />
+          <TextInput placeholder="Enter id here" alignSelf='center' textAlign='center' margin='none'
+            onChange={event => this.setState({ userInput: event.target.value })} />
+          <Button label="Enter" onClick={this.sendThing.bind(this)} />
         </Box>
       )
-    } else if (this.state.idState == 'failed') {
+    } else if (this.state.idState === 'failed') {
       uninit = (
         <Box align='center' justify='center' background='black' gap='small'>
           <Heading level='1' alignSelf='center' textAlign='center' margin='none'>Enter your Protocol 29 id:</Heading>
-          <TextInput placeholder="Enter id here" alignSelf='center' textAlign='center' margin='none' />
-          <Text level='4' color="fail" alignSelf='center' textAlign='center' margin='none'>Failed! {this.state.selectedId} doesn't exist</Text>
-          <Button label="Enter" />
+          <TextInput placeholder="Enter id here" alignSelf='center' textAlign='center' margin='none'
+            onChange={event => this.setState({ userInput: event.target.value })} />
+          <Text level='4' color="fail" alignSelf='center' textAlign='center' margin='none'>Failed! {this.state.userInput} doesn't exist</Text>
+          <Button label="Enter" onClick={this.sendThing.bind(this)} />
         </Box>
       )
-    } else if (this.state.idState == 'found') {
+    } else if (this.state.idState === 'found') {
       //TODO:  Display the team + Name + ID
       uninit = (
         <Box align='center' justify='center' background='black' gap='small'>
-          <Heading level='1' alignSelf='center' textAlign='center' margin='none'>Enter your Protocol 29 id:</Heading>
-          <TextInput placeholder="Enter id here" alignSelf='center' textAlign='center' margin='none' />
-          <Text level='4' color="fail" alignSelf='center' textAlign='center' margin='none'>Failed! {this.state.selectedId} doesn't exist</Text>
+          <Text level='4' alignSelf='center' textAlign='center' margin='none'>Welcome {this.toTitleCase(this.state.playerName)}! You belong to the {this.toTitleCase(this.state.team)} Clan</Text>
           <Button label="Enter" />
         </Box>
       )
